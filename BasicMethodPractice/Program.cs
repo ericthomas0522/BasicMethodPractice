a8a3ae1fac24a5b052dc710d227f194b782b4680
Eric Thomas <eric.therock@gmail.com>
2018-07-18 22:03:53 -0500

    Method practice from chapter 15 of the C# players guide. Used the Array.Reverse method instead of writing my own since it already existed

--- BasicMethodPractice/Program.cs
+++ BasicMethodPractice/Program.cs
@@ -10,6 +10,34 @@ namespace BasicMethodPractice
     {
         static void Main(string[] args)
         {
+            int[] numbers = GenerateNumbers(20);
+            Array.Reverse(numbers);
+            PrintNumbers(numbers);
+            Console.ReadLine();
         }
+
+         static int[] GenerateNumbers(int arrayLength)
+         {
+         int[] arrayToReturn = new int[arrayLength];
+        
+          for (int index = 0; index < arrayToReturn.Length; index++)
+         {
+             arrayToReturn[index] = index+1;
+         }
+
+         return arrayToReturn;
+
+        }
+
+
+        static void PrintNumbers(int[] arrayToPrint)
+        {
+            for (int index = 0; index < arrayToPrint.Length; index++)
+            {
+                Console.Write(arrayToPrint[index] + ", ");
+            }
+                
+        }
+        
     }
 }
